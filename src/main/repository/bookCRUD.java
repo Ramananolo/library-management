f38c0ce19b6b05bc0782ce50e6441e57cdcd8f85
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2023-11-19 23:23:00 +0300

    add repository of book

---
+++ src/main/repository/bookCRUD.java
@@ -0,0 +1,33 @@
+package repository;
+
+import entiter.book;
+
+public class bookCRUD implements CrudOperations<Book> {
+    private Connection connection;
+
+    public bookCRUD(Connection connection) {
+        this.connection = connection;
+    }
+    @Override
+    public List<Book> findAll() {
+        List<Book> books = new ArrayList<>();
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM \"Books\";")) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                int pageNumber = resultSet.getInt("pageNumber");
+                Date releaseDate = resultSet.getDate("releaseDate");
+                int idAuthor = resultSet.getInt("idAuthor");
+                Book.Topic topicValue = Book.Topic.valueOf(resultSet.getString("topic"));
+                String status = resultSet.getString("status");
+                Book book = new Book(id, name, pageNumber, releaseDate, idAuthor,topicValue, status);
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return books;
+    }
+}
